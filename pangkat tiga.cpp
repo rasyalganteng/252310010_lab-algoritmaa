ad86f3dea4ac2893c5fb49f75a562dcf35404b1b
rasyalganteng <zxrasyal@gmail.com>
2025-12-14 20:27:49 +0700

    Add files via upload

---
+++ pangkat tiga.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void pangkatTiga(int &x) {
+    x = x * x * x;   
+}
+
+int main() {
+    int nilai;
+
+    cout << "Masukkan nilai: ";
+    cin >> nilai;
+    pangkatTiga(nilai);
+    cout << "Hasil pangkat tiga: " << nilai << endl;
+
+    return 0;
+}
